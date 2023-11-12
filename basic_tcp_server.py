aaa15b104a10e5ec99a27d47b838822c240a7a61
sjbavier <sjbavier@gmail.com>
2023-11-12 18:52:37 -0500

    initial revisiting

--- basic_tcp_server.py
+++ basic_tcp_server.py
@@ -9,7 +9,7 @@ server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((bind_ip,bind_port))
 
 
-server.listen(5)
+server.listen(5) # maximum backlog of 5 connections
 
 print("[*] Listening on %s:%d" % (bind_ip, bind_port))
 
@@ -18,17 +18,18 @@ def handle_client(client_socket):
 
     # print what the client sends
     request = client_socket.recv(1024)
-
-    print("[*] Received: %s" % request)
+    request_utf8 = request.decode('utf-8')
+    print(f"[*] Received: {request_utf8}")
 
     # send a packet
-    client_socket.send("Send packet")
+    response_str = "Here is your response".encode('utf-8', errors='ignore')
+    client_socket.send(response_str)
 
     client_socket.close()
 
 while True:
 
-    client,addr = server.accept()
+    client, addr = server.accept()
 
     print("[*] Accepted connection from: %s:%d" % (addr[0],addr[1]))
 
